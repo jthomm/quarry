2abd4f7cad7a80e68f926a0bf47bccd497c42f9f
jthomm <jthomm@yahoo.com>
2013-04-08 18:16:29 -0300

    Update quarry.js

--- quarry.js
+++ quarry.js
@@ -1,15 +1,13 @@
 (function () {
 
-  /* Requisite helpers */
+  /* Requisite functions */
 
   var root = this
-    , slice = Array.prototype.slice
     , shift = Array.prototype.shift
-    , hasOwn = Object.prototype.hasOwnProperty
+    , slice = Array.prototype.slice
   ;
 
   function objectSlice(object, properties) {
-    // Requires `hasOwn`
     var property
       , i = 0
       , n = properties.length
@@ -18,11 +16,47 @@
     while (i < n) {
       property = properties[i];
       i += 1;
-      if (hasOwn.call(object, property)) {
+      if (object.hasOwnProperty(property)) {
         result[property] = object[property];
       }
     }
     return result;
+  }
+
+  function mergeObjects() {
+    var objects = slice.call(arguments)
+      , object
+      , i = 0
+      , n = objects.length
+      , property
+      , result = {}
+    ;
+    while (i < n) {
+      object = objects[i];
+      i += 1;
+      for (property in object) {
+        if (object.hasOwnProperty(property)) {
+          result[property] = object[property];
+        }
+      }
+    }
+    return result;
+  }
+
+  function getValues(objects, property) {
+    var object
+      , i = 0
+      , n = this.length
+      , results = []
+    ;
+    while (i < n) {
+      object = objects[i];
+      i += 1;
+      if (object.hasOwnProperty(property)) {
+        results.append(object[property]);
+      }
+    }
+    return results;
   }
 
 
@@ -30,39 +64,37 @@
   /* Main course */
 
   function select() {
-    // Requires `shift`
-    var query = shift.call(arguments)
-      , item
+    var object
       , i = 0
       , n = this.length
       , results = []
+      , query = shift.call(arguments)
     ;
     if (typeof query === 'undefined') {
       return this;
     }
     while (i < n) {
-      item = this[i];
+      object = this[i];
       i += 1;
-      if (smartmatch(item, query)) {
-        results.push(item);
+      if (smartmatch(object, query)) {
+        results.push(object);
       }
     }
     return select.apply(results, arguments);
   }
 
   function project() {
-    // Requires `slice`, `objectSlice`
-    var properties = slice.call(arguments)
-      , item
+    var object
       , i = 0
       , n = this.length
-      , sliced
       , results = []
+      , sliced
+      , properties = slice.call(arguments)
     ;
     while (i < n) {
-      item = this[i];
+      object = this[i];
       i += 1;
-      sliced = objectSlice(item, properties);
+      sliced = objectSlice(object, properties);
       if (!smartmatch(results, sliced)) {
         results.push(sliced);
       }
@@ -71,20 +103,47 @@
   }
 
   function groupBy() {
-    // Requires `project`, `slice`
-    var projected = project.apply(this, arguments)
-      , item
+    var object
       , i = 0
-      , n = projected.length
-      , selected
+      , n = this.length
+      , results = []
+      , sliced
+      , group
+      , properties = slice.call(arguments)
     ;
     while (i < n) {
-      item = projected[i];
-      selected = this.select(item);
-      item._data = selected;
+      object = this[i];
       i += 1;
+      sliced = objectSlice(object, properties);
+      group = select.call(results, sliced)[0];
+      if (typeof group === 'undefined') {
+        sliced.$ = [object];
+        results.push(sliced);
+      } else {
+        group.$.push(object);
+      }
     }
-    return projected;
+    return results;
+  }
+
+  function join(objects, properties) {
+    var object
+      , i = 0
+      , n = this.length
+      , results = []
+      , sliced
+      , source
+    ;
+    while (i < n) {
+      object = this[i];
+      i += 1;
+      sliced = objectSlice(object, properties);
+      source = select.call(objects, sliced)[0];
+      if (typeof source !== 'undefined') {
+        results.push(mergeObjects(object, source));
+      }
+    }
+    return results;
   }
 
 
@@ -95,6 +154,7 @@
     this.select = select;
     this.project = project;
     this.groupBy = groupBy;
+    this.join = join;
     return this;
   }
 
@@ -102,6 +162,6 @@
     return asQuarry.call(data.slice(0));
   }
 
-  root.Q = Q;
+  root.Q = Q
 
 }).call(this);
